b620feb4a0d27d43814a86f40b12e581538dc292
Daniel Yorwarth <daniel@pagecreative.co.uk>
2020-01-15 17:06:47 +0000

    Added compression

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,13 @@ module.exports = {
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
     },
+    // Compression 
+    {
+      resolve: 'gatsby-plugin-brotli',
+      options: {
+        extensions: ['css', 'html', 'js', 'svg']
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
